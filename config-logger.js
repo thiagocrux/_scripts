12dac0bac5368498e100b30d7294987676e852da
Thiago Cruz <thiagocruz.eu@gmail.com>
2025-04-03 14:41:58 -0300

    fix: add missing dependency to script execution

--- config-logger.js
+++ config-logger.js
@@ -3,26 +3,30 @@ const fs = require('node:fs');
 
 const SETUP_PATH = process.cwd();
 
-exec('pnpm add pino', (error, stdout, stderr) => {
-  console.log(stdout);
-  console.log(stderr);
+exec(
+  `
+    pnpm add pino
+    pnpm add pino-pretty -D
+  `,
+  (error, stdout, stderr) => {
+    console.log(stdout);
+    console.log(stderr);
 
-  if (error) {
-    console.log(`exec err`, error);
-    return;
-  }
-
-  // Create 'src' folder and 'index.ts' file
-  fs.mkdir(`${SETUP_PATH}/src/utils`, { recursive: true }, (error) => {
     if (error) {
-      console.error('Error creating folder:', error);
+      console.log(`exec err`, error);
       return;
     }
 
-    fs.writeFile(
-      `${SETUP_PATH}/src/utils/logger.ts`,
-      `
-        import { formatRFC3339 } from 'date-fns';
+    // Create 'src' folder and 'index.ts' file
+    fs.mkdir(`${SETUP_PATH}/src/utils`, { recursive: true }, (error) => {
+      if (error) {
+        console.error('Error creating folder:', error);
+        return;
+      }
+
+      fs.writeFile(
+        `${SETUP_PATH}/src/utils/logger.ts`,
+        `
         import pino from 'pino';
 
         const logger = pino({
@@ -30,7 +34,7 @@ exec('pnpm add pino', (error, stdout, stderr) => {
             target: 'pino-pretty',
             options: {
               colorize: true,
-              translateTime: \`\${JSON.stringify(formatRFC3339(new Date()))}\`,
+              translateTime: \`\${JSON.stringify(new Date().toLocaleString())}\`,
             },
           },
           base: {
@@ -40,26 +44,27 @@ exec('pnpm add pino', (error, stdout, stderr) => {
 
         export default logger;
       `,
-      'utf8',
-      (error) => {
-        if (error) {
-          console.log('Error writing file:', error);
-          return;
-        }
+        'utf8',
+        (error) => {
+          if (error) {
+            console.log('Error writing file:', error);
+            return;
+          }
 
-        console.log(`File 'logger.ts' created successfully!`);
+          console.log(`File 'logger.ts' created successfully!`);
+        }
+      );
+    });
+
+    // Formatting all files with prettier
+    exec(
+      'npx prettier src/utils/logger.ts --write --single-quote',
+      (error, stdout, stderr) => {
+        console.log('\n');
+        console.log(stdout);
+        console.log(stderr);
+        error && console.log(`[prettier] Error:`, error);
       }
     );
-  });
-
-  // Formatting all files with prettier
-  exec(
-    'npx prettier src/utils/logger.ts --write --single-quote',
-    (error, stdout, stderr) => {
-      console.log('\n');
-      console.log(stdout);
-      console.log(stderr);
-      error && console.log(`[prettier] Error:`, error);
-    }
-  );
-});
+  }
+);
