ca484470c1f4d8a509e3bea446f5af43fe249c27
Thiago Cruz <thiagocruz.eu@gmail.com>
2025-04-03 14:30:42 -0300

    feat: create config-logger script

---
+++ config-logger.js
@@ -0,0 +1,65 @@
+const { exec } = require('child_process');
+const fs = require('node:fs');
+
+const SETUP_PATH = process.cwd();
+
+exec('pnpm add pino', (error, stdout, stderr) => {
+  console.log(stdout);
+  console.log(stderr);
+
+  if (error) {
+    console.log(`exec err`, error);
+    return;
+  }
+
+  // Create 'src' folder and 'index.ts' file
+  fs.mkdir(`${SETUP_PATH}/src/utils`, { recursive: true }, (error) => {
+    if (error) {
+      console.error('Error creating folder:', error);
+      return;
+    }
+
+    fs.writeFile(
+      `${SETUP_PATH}/src/utils/logger.ts`,
+      `
+        import { formatRFC3339 } from 'date-fns';
+        import pino from 'pino';
+
+        const logger = pino({
+          transport: {
+            target: 'pino-pretty',
+            options: {
+              colorize: true,
+              translateTime: \`\${JSON.stringify(formatRFC3339(new Date()))}\`,
+            },
+          },
+          base: {
+            pid: false,
+          },
+        });
+
+        export default logger;
+      `,
+      'utf8',
+      (error) => {
+        if (error) {
+          console.log('Error writing file:', error);
+          return;
+        }
+
+        console.log(`File 'logger.ts' created successfully!`);
+      }
+    );
+  });
+
+  // Formatting all files with prettier
+  exec(
+    'npx prettier src/utils/logger.ts --write --single-quote',
+    (error, stdout, stderr) => {
+      console.log('\n');
+      console.log(stdout);
+      console.log(stderr);
+      error && console.log(`[prettier] Error:`, error);
+    }
+  );
+});
