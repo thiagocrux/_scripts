eefdcc20b0533c80b6915d64f22ac0916ea0529c
Thiago Cruz <thiagocruz.eu@gmail.com>
2025-04-16 11:15:44 -0300

    feat: add json middleware to node with ts config script

--- config-node-with-typescript.js
+++ config-node-with-typescript.js
@@ -35,8 +35,9 @@ exec(
 
           const app = express();
 
+          app.use(express.json());
+
           app.listen(3001, () => {
-            // eslint-disable-next-line no-console
             console.log(\`Server running on http://localhost:3001\`);
           });
         `,
